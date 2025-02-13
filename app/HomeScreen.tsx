7b4e4bfc90da44f32739e076eafe2f01d6708973
Parth Shekhaliya <parthshekhaliya18@gmail.com>
2025-02-13 19:26:03 +0530

    - custom fonts, fontSize, colors setup with nativewind

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 const HomeScreen: React.FC = () => {
+  console.log('HELLO');
   return (
-    <SafeAreaView className="flex-1 bg-white justify-center items-center">
-      <Text className="text-red-500 font-bold">Hello world!</Text>
+    <SafeAreaView className="flex-1 bg-color-blue06 justify-center items-center">
+      <Text className="text-color-caution font-poppins700 text-medium-extra p-3 border-color-white border-2 rounded-[10] uppercase dark:text-color-success">
+        Hello world!
+      </Text>
     </SafeAreaView>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,48 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: ['./App.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins100: ['Poppins-Thin'],
+        poppins300: ['Poppins-Light'],
+        poppins400: ['Poppins-Regular'],
+        poppins500: ['Poppins-Medium'],
+        poppins600: ['Poppins-SemiBold'],
+        poppins700: ['Poppins-Bold'],
+      },
+      fontSize: {
+        'very-tiny': '6px',
+        tiny: '8px',
+        'small-tiny': '10px',
+        small: '12px',
+        'small-medium': '14px',
+        medium: '16px',
+        'medium-extra': '18px',
+        regular: '20px',
+        'regular-extra': '22px',
+        large: '24px',
+        'extra-large': '48px',
+      },
+      colors: {
+        color: {
+          white: '#FFFFFF',
+          black: '#000000',
+          blue: '#0094A9',
+          blue01: '#0094A9',
+          blue02: '#0C879D',
+          blue03: '#197890',
+          blue04: '#266983',
+          blue05: '#3D5F7A',
+          blue06: '#4A5974',
+          error: '#FA4B44',
+          success: '#0E9B85',
+          caution: '#ECC30B',
+        },
+      },
+    },
   },
   plugins: [],
 };
