e1ab9b0b1d389539c794625e14ed84b9328bbbcb
Parth Shekhaliya <parthshekhaliya18@gmail.com>
2025-02-19 11:02:15 +0530

    - setup for react native svg icons

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Icon from './assets/icons/verified.svg';
+import {cssInterop} from 'nativewind';
+
+cssInterop(Icon, {
+  className: {
+    target: 'style',
+    nativeStyleToProp: {width: true, height: true},
+  },
+});
 
 const HomeScreen: React.FC = () => {
-  console.log('HELLO');
   return (
     <SafeAreaView className="flex-1 bg-color-blue06 justify-center items-center">
-      <Text className="text-color-caution font-poppins700 text-medium-extra p-3 border-color-white border-2 rounded-[10] uppercase dark:text-color-success">
+      <Icon className="m-2" />
+      <Text className="text-color-caution font-poppins700 text-medium p-3 border-color-white border-2 rounded-3 uppercase dark:text-color-success">
         Hello world!
       </Text>
     </SafeAreaView>
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const {withNativeWind} = require('nativewind/metro');
+const defaultConfig = getDefaultConfig(__dirname);
+const {assetExts, sourceExts} = defaultConfig.resolver;
 
 /**
  * Metro configuration
@@ -7,8 +9,19 @@ const {withNativeWind} = require('nativewind/metro');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = mergeConfig(getDefaultConfig(__dirname), {
-  /* your config */
-});
+
+const svgConfig = {
+  transformer: {
+    babelTransformerPath: require.resolve(
+      'react-native-svg-transformer/react-native',
+    ),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+const config = mergeConfig(defaultConfig, svgConfig);
 
 module.exports = withNativeWind(config, {input: './global.css'});
